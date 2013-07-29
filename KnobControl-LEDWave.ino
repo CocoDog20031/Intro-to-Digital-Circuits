0db33175491e37390f6a3ac75e93c32b401ab6c5
CocoDog20031 <shubhi1us@gmail.com>
2013-07-29 15:10:49 -0700

    Create KnobControl-LEDWave.ino

---
+++ KnobControl-LEDWave.ino
@@ -0,0 +1,20 @@
+
+void setup()
+{
+  for( int t = 2; t <= 5 ; t++)
+ {
+ pinMode(t,OUTPUT); 
+ }
+  
+  
+}
+
+void loop()
+{        
+for( int t = 2; t <= 5 ; t++)
+ {
+ digitalWrite(t,HIGH);
+ delay( analogRead(A0));
+ digitalWrite(t,LOW);
+ }
+}
